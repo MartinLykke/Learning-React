957df8d3bc60bd80ec0d0d7748259cb97b2e5f99
MartinLykke <32263396+MartinLykke@users.noreply.github.com>
2021-01-20 15:40:15 +0100

    book list

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
+// CSS
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function BookList() {
+  return (
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Title></Title>
+      <Author></Author>
+    </article>
+  );
+};
+const Image = () => (
+  <img
+    src='https://images-na.ssl-images-amazon.com/images/I/41OiCBwqgsL.jpg'
+    alt=''
+  />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Title = () => <h1>Software Engineering</h1>;
+const Author = () => (
+  <h1 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
+    Ian Sommerville
+  </h1>
+);
+ReactDom.render(<BookList />, document.getElementById("root"));
