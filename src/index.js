772b8272633bf4236642644993776820f9b1ee99
MartinLykke <32263396+MartinLykke@users.noreply.github.com>
2021-01-21 12:06:14 +0100

    book list basics

--- src/index.js
+++ src/index.js
@@ -3,37 +3,19 @@ import ReactDom from "react-dom";
 // CSS
 import "./index.css";
 
+import { data } from "./books";
+import SpecificBook from "./Book";
+import { greeting } from "./testing/testing";
+
 function BookList() {
+  console.log(greeting);
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {data.map((book, index) => {
+        return <SpecificBook key={book.id} {...book}></SpecificBook>;
+      })}
     </section>
   );
 }
-const Book = () => {
-  return (
-    <article>
-      <Image></Image>
-      <Title></Title>
-      <Author></Author>
-    </article>
-  );
-};
-const Image = () => (
-  <img
-    src='https://images-na.ssl-images-amazon.com/images/I/41OiCBwqgsL.jpg'
-    alt=''
-  />
-);
-const Title = () => <h1>Software Engineering</h1>;
-const Author = () => (
-  <h1 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-    Ian Sommerville
-  </h1>
-);
+
 ReactDom.render(<BookList />, document.getElementById("root"));
